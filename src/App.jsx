79ca620f73a05ed61a1f5b7f5662285ae68fce99
Dario Ortega <dariolean10@gmail.com>
2022-11-03 23:28:25 -0300

    creacion app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Box display={"flex"} justifyContent={"center"}> hola mundo</Box>
+    </div>
+  )
+}
+
+export default App
